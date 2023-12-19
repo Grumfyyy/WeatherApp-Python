e0d3fb292507a6d0fb6bc0210da979790b6b26ec
Grumfyyy <swadijosef@gmail.com>
2023-12-19 20:07:37 +0100

    La till lite bilder, en på idén för layout till applikationen och en progress bild, samt så displayar jag temperatur i form av en Label och detta visas dynamiskt utifrån stad man söker på.

--- weather-app.py
+++ weather-app.py
@@ -17,62 +17,77 @@ import datetime
 #delete file - https://www.w3schools.com/python/python_file_remove.asp
 import os
 
-def get_weather_data(latitude, longitude, city_name):
-    # https://open-meteo.com/en/docs
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m&hourly=temperature_2m&forecast_days=1"
-    result = requests.get(url)
+class WeatherApp():
 
-    # Serializing json
-    json_object = json.dumps(result.json(), indent=4)
-    
-    #writing to json data to a json file
-    with open("output.json", "w") as outfile:
-        outfile.write(json_object)
+    def __init__(self):
+        self.temperature = ""
 
-    #read and load the json file, that was just created
-    f = open('output.json',)
-    json_result = json.load(f)
+    def get_weather_data(self, latitude, longitude, city_name):
+        # https://open-meteo.com/en/docs
+        url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m&hourly=temperature_2m&forecast_days=1"
+        result = requests.get(url)
 
-    #close and delete the file
-    f.close()
-    os.remove('output.json')
+        # Serializing json
+        json_object = json.dumps(result.json(), indent=4)
+        
+        #writing to json data to a json file
+        with open("output.json", "w") as outfile:
+            outfile.write(json_object)
 
-    #print(json_result['hourly']['temperature_2m'])
+        #read and load the json file, that was just created
+        f = open('output.json',)
+        json_result = json.load(f)
 
-    #get the current time, but i only need the current hour of the current day since i have a list of 24 values, because we have 24 hours
-    current_time = datetime.datetime.now()
-    current_hour = current_time.strftime('%H')
+        #close and delete the file
+        f.close()
+        os.remove('output.json')
 
-    temperature_list = json_result['hourly']['temperature_2m']
-    current_temperature = temperature_list[int(current_hour)] # get the current temperature from the list of 24 values with the help of the current hour as an index
+        #print(json_result['hourly']['temperature_2m'])
 
-    #print out the current temperature to the prompt
-    print(f'current temperature in {city_name} = {current_temperature} \u00b0C ')
-    
+        #get the current time, but i only need the current hour of the current day since i have a list of 24 values, because we have 24 hours
+        current_time = datetime.datetime.now()
+        current_hour = current_time.strftime('%H')
 
-def get_coordinates(city_name):
-    geolocator = Nominatim(user_agent="WeatherApp")
+        temperature_list = json_result['hourly']['temperature_2m']
+        current_temperature = temperature_list[int(current_hour)] # get the current temperature from the list of 24 values with the help of the current hour as an index
 
-    location = geolocator.geocode(city_name)
+        #print out the current temperature to the prompt
+        #print(f'current temperature in {city_name} = {current_temperature} \u00b0C ')
 
-    get_weather_data(location.latitude, location.longitude, city_name)
-    #print(f"The latitude of {city_name} is: ", location.latitude)
-    #print(f"The longitude of {city_name} is: ", location.longitude)
+        self.temperature = str(current_temperature)
+        self.temperatureText.set(f"Current temperature in {city_name} is : {current_temperature} \u00b0C")
+        
+    def get_coordinates(self, city_name):
+        geolocator = Nominatim(user_agent="WeatherApp")
 
-def main():
-    root = Tk()
-    root.title("Weather Application by Josef Swadi Johansson")
-    root.geometry("400x400")
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-    ttk.Label(frm, text="City : ").grid(column=0, row = 0)
+        location = geolocator.geocode(city_name)
 
-    city_input = ttk.Entry(root)
-    city_input.grid(column=1, row = 0)
-    
-    ttk.Button(frm, text="Search", command=(lambda:get_coordinates(city_input.get()))).grid(column = 2, row=0)
+        self.get_weather_data(location.latitude, location.longitude, city_name)
+        #print(f"The latitude of {city_name} is: ", location.latitude)
+        #print(f"The longitude of {city_name} is: ", location.longitude)
 
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column = 0, row=2)
-    root.mainloop()
+    def updateTemperatureText(self):
+        pass
 
-main()
+    def main(self):
+        root = Tk()
+        root.title("Weather Application by Josef Swadi Johansson")
+        root.geometry('400x400')
+
+        ttk.Label(root, text="City : ").place(x=5, y=5)
+
+        city_input = ttk.Entry(root)
+        city_input.place(x=50, y=5)
+        
+        search_button = ttk.Button(root, text="Search", command=(lambda:self.get_coordinates(city_input.get())))
+        search_button.place(x = 200, y=5)
+
+        self.temperatureText = StringVar()
+
+        temperature_label = ttk.Label(root, textvariable=self.temperatureText)
+        temperature_label.place(x=5, y=30)
+
+        root.mainloop()
+
+app = WeatherApp()
+app.main()
