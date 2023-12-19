b786fb66d079f6728bb811e8d8bf022e0b6de7ef
Grumfyyy <swadijosef@gmail.com>
2023-12-19 18:09:59 +0100

    I nuvarande form så använder jag open-meteo väder api, för få en väder prognos för dagen, så man skickar in koordinaterna får dagens prognos som skrivs ut i kommandoprompten.

--- weather-app.py
+++ weather-app.py
@@ -6,13 +6,42 @@ from tkinter import ttk
 #Geopy example - https://www.tutorialspoint.com/how-to-get-the-longitude-and-latitude-of-a-city-using-python
 from geopy.geocoders import Nominatim
 
+#requests - https://www.w3schools.com/python/module_requests.asp
+import requests 
+#json documentation - https://www.w3schools.com/python/python_json.asp
+import json
+
+#datetime documentation - https://www.w3schools.com/python/python_datetime.asp
+import datetime
+
+def get_weather_data(latitude, longitude):
+    # https://open-meteo.com/en/docs
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m&hourly=temperature_2m&forecast_days=1"
+    result = requests.get(url)
+
+    # Serializing json
+    json_object = json.dumps(result.json(), indent=4)
+    
+    # Writing to sample.json
+    with open("sample.json", "w") as outfile:
+        outfile.write(json_object)
+
+    f = open('sample.json',)
+    json_result = json.load(f)
+
+    f.close()
+
+    print(json_result['hourly']['temperature_2m'])
+    
+
 def get_coordinates(city_name):
     geolocator = Nominatim(user_agent="WeatherApp")
 
     location = geolocator.geocode(city_name)
 
-    print(f"The latitude of {city_name} is: ", location.latitude)
-    print(f"The longitude of {city_name} is: ", location.longitude)
+    get_weather_data(location.latitude, location.longitude)
+    #print(f"The latitude of {city_name} is: ", location.latitude)
+    #print(f"The longitude of {city_name} is: ", location.longitude)
 
 def main():
     root = Tk()
