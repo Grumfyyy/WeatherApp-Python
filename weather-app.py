4268bae2f96888d875fa918ec01a6d0d31fccf6d
Grumfyyy <swadijosef@gmail.com>
2023-12-19 12:55:49 +0100

    La till ett fält, en knapp, samt en funktion för att kunna söka efter koordinaterna, dock får man inte ut koordinaterna utifrån stad ännu, utan den printar bara ut stadens namn för säkerställa att informationen skickas rätt.

--- weather-app.py
+++ weather-app.py
@@ -1,10 +1,25 @@
 #tkinter documentation - https://docs.python.org/3/library/tkinter.html
+#tkinter entry documentation - https://python-course.eu/tkinter/entry-widgets-in-tkinter.php
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World").grid(column=0, row = 0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column = 1, row=0)
-root.mainloop()
+def get_coordinates(city_name):
+    print(city_name)
+
+def main():
+    root = Tk()
+    root.title("Weather Application by Josef Swadi Johansson")
+    root.geometry("400x400")
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="City : ").grid(column=0, row = 0)
+
+    city_input = ttk.Entry(root)
+    city_input.grid(column=1, row = 0)
+    
+    ttk.Button(frm, text="Search", command=(lambda:get_coordinates(city_input.get()))).grid(column = 2, row=0)
+
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column = 0, row=2)
+    root.mainloop()
+
+main()
